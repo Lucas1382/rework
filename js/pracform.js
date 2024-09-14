a99a269af7462a519cc650e30f44fda093a066ad
Lucas1382 <nguyendangduclong@gmail.com>
2024-09-14 20:56:27 +1000

    done

--- js/pracform.js
+++ js/pracform.js
@@ -1,22 +1,34 @@
 const temporaryYes = document.querySelector('#temporary-yes')
 const temporaryNo = document.querySelector('#temporary-no')
 const temporaryBlock = document.querySelector('#if-temporary')
+const agreeCheckbox = document.querySelector("#agree-checkbox")
+const submitBtn = document.querySelector(".submit-btn")
+const specificClinic = document.querySelector("#other-disability")
 
 temporaryBlock.style.display = 'none'
 
 temporaryYes.addEventListener('change', () => {
     if (temporaryYes.checked) {
         temporaryBlock.style.display = 'block'
+        specificClinic.required = 'true'
     }
 })
 
 temporaryNo.addEventListener('change', () => {
     if (temporaryNo.checked) {
         temporaryBlock.style.display = 'none'
+        specificClinic.required = 'false'
     }
 })
 
-document.getElementById('prac-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Are you sure?');
+
+agreeCheckbox.addEventListener("change", function() {
+    if (agreeCheckbox.checked) {
+        submitBtn.disabled = false; // Enable the submit button
+        document.querySelector('.confirmation p').innerHTML = ''
+    } else {
+        submitBtn.disabled = true;  // Disable the submit button
+        document.querySelector('.confirmation p').innerHTML = '<span>X</span> Please agree detail'
+    }
 });
+
--- js/userform.js
+++ js/userform.js
@@ -1,54 +1,91 @@
+const otherNameYes = document.querySelector("#other-name-yes")
+const otherNameNo = document.querySelector("#other-name-no")
+const otherNameBlock = document.querySelector("#if-other-name")
+const otherNameSpecific = document.querySelector("#other-name")
 const suspendYes= document.querySelector("#suspended-no")
 const suspendedNo = document.querySelector("#suspended-yes")
 const suspendedBlock = document.querySelector("#if-suspended")
+const suspendedInputs = document.querySelectorAll("#if-suspended input")
 const newApplication = document.querySelector('#new')
 const renewalApplication = document.querySelector('#renewal')
 const replacementApplication = document.querySelector('#replacement')
 const replacementBlock = document.querySelector("#if-replace")
-const temporaryYes = document.getElementById("temporary-yes")
-const temporaryNo = document.querySelector("#temporary-no")
-const temporaryBlock = document.querySelector("#if-temporapy")
+const replacementReason = document.querySelector('input[name="replace_reason"]')
+const agreeCheckbox = document.querySelector("#agree-checkbox")
+const submitBtn = document.querySelector(".submit-btn")
 
 
-document.getElementById('userform').reset();
+// Make sure all hidden div element is invisible by default
 suspendedBlock.style.display = 'none'
 replacementBlock.style.display = 'none'
 
-suspendedNo.addEventListener("change", () => {
-    if(suspendedNo.checked) {
-        suspendedBlock.style.display = 'block'
+// Turn on/off other name block
+otherNameYes.addEventListener("change", () => {
+    if(otherNameYes.checked) {
+        otherNameBlock.style.display = 'block'
+        otherNameSpecific.required = 'true'
     }
 })
 
-suspendYes.addEventListener("change", () => {
-    if(suspendYes.checked) {
-        suspendedBlock.style.display = 'none'
+otherNameNo.addEventListener("change", () => {
+    if(otherNameNo.checked) {
+        otherNameBlock.style.display = 'none'
+        otherNameSpecific.required = 'false'
     }
 })
 
 
 
-
-replacementApplication.addEventListener("change", () => {
+// Turn on/off replacement reason block
+replacementApplication.addEventListener("change", () => { 
     if(replacementApplication.checked) {
         replacementBlock.style.display = 'block'
+        replacementReason.required = 'true'
     }
 })
 
 newApplication.addEventListener("change", () => {
     if(newApplication.checked) {
         replacementBlock.style.display = 'none'
+        replacementReason.required = 'false'
     }
 })
 
 renewalApplication.addEventListener("change", () => {
     if(renewalApplication.checked) {
         replacementBlock.style.display = 'none'
+        replacementReason.required = 'false'
     }
 })
-document.getElementById('userform').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Are you sure?');
+
+
+// Turn on/off suspended 
+suspendedNo.addEventListener("change", () => {
+    if(suspendedNo.checked) {
+        suspendedBlock.style.display = 'block'
+        suspendedInputs.forEach(input => {
+            input.required = 'true'
+        })
+    }
+})
+
+suspendYes.addEventListener("change", () => {
+    if(suspendYes.checked) {
+        suspendedBlock.style.display = 'none'
+        suspendedInputs.forEach(input => {
+            input.required = 'false'
+        })
+    }
+})
+
+agreeCheckbox.addEventListener("change", function() {
+    if (agreeCheckbox.checked) {
+        submitBtn.disabled = false; // Enable the submit button
+        document.querySelector('.confirmation p').innerHTML = ''
+    } else {
+        submitBtn.disabled = true;  // Disable the submit button
+        document.querySelector('.confirmation p').innerHTML = '<span>X</span> Please agree detail'
+    }
 });
 
 
